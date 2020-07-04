bb87950b7dc7e80437af04855fc740215fce7a09
Carlos Campo <carloscampo5200@gmail.com>
2020-07-03 20:39:23 -0500

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 """ So that the reverse function can see the appropiate app"""
 app_name = 'recipe'
